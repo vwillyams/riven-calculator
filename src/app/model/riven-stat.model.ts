d026491effc499f15c65fc0ac6c7aa8aa33da424
Victor <victorw@unity3d.com>
2018-04-30 12:46:58 -0500

    using getter property to ensure that desirabilities stay in sync

--- src/app/model/riven-stat.model.ts
+++ src/app/model/riven-stat.model.ts
@@ -1,12 +1,47 @@
+import {StatDesirability} from '../const/stat-desirability';
+
 export class RivenStat {
   name: string;
   prefix?: string;
   suffix?: string;
   restrict?: string;
-  posDesirability?: string;
-  negDesirability?: string;
   negativeBlocked: boolean;
   class: string;
+
+  private _posDesirability?: string;
+  private _negDesirability?: string;
+
+  get posDesirability(): string {
+    return this._posDesirability;
+  }
+
+  set posDesirability(desirability: string) {
+    this._posDesirability = desirability;
+    this.updateOpposite(desirability, '_negDesirability', 'required-positive');
+  }
+
+  get negDesirability(): string {
+    return this._negDesirability;
+  }
+
+  set negDesirability(desirability: string) {
+    this._negDesirability = desirability;
+    this.updateOpposite(desirability, '_posDesirability', 'required-negative');
+  }
+
+  private updateOpposite(desirability: string, oppType: string, targetClass: string) {
+    if (desirability === StatDesirability.plusPlus) {
+      this[oppType] = StatDesirability.minus;
+      this.class = targetClass;
+    } else if (this[oppType] === StatDesirability.plusPlus) {
+      if (desirability === StatDesirability.plus) {
+        this[oppType] = StatDesirability.plus;
+        this.class = null;
+      }
+    } else {
+      this.class = null;
+    }
+  }
 
   constructor(obj: Object) {
     if (obj) {
